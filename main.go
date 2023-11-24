ba09082f9c5f443c75bf74a3fa078bedc10244bd
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2023-11-24 23:30:33 +0530

    Support HMR, move around renderer files

--- main.go
+++ main.go
@@ -21,35 +21,16 @@ import (
 // TODO: Validate written image.
 // LOW-TODO: Future support for flashing to an internal drive?
 
-const html = `
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <!-- Use minimum-scale=1 to enable GPU rasterization -->
-  <meta
-    name='viewport'
-    content='user-scalable=0, initial-scale=1, minimum-scale=1, width=device-width, height=device-height'
-  />
-	<style>
-	body {
-		margin: 0;
-		font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",
-		  Ubuntu,Cantarell,Oxygen-Sans,"Helvetica Neue",Arial,Roboto,sans-serif;
-	}
-  </style>
-</head>
-<body><div id="app"></div><script>initiateReact()</script></body>
-</html>
-`
-
 const version = "1.0.0-alpha.2"
 
 var w webview.WebView
 
+//go:embed renderer/index.html
+var html string
+var overrideUrl = ""
+
 //go:embed dist/index.js
 var js string
-
-// var file = ""
 
 // ParseToJsString takes a string and escapes slashes and double-quotes,
 // and converts it to a string that can be passed to JavaScript.
@@ -224,6 +205,11 @@ func main() {
 		}
 	})
 
-	w.Navigate("data:text/html," + html)
+	if overrideUrl != "" {
+		w.Navigate(overrideUrl)
+	} else {
+		w.Navigate("data:text/html," + strings.ReplaceAll(html,
+			"<script type=\"module\" src=\"./index.tsx\" />", "<script>initiateReact()</script>"))
+	}
 	w.Run()
 }
